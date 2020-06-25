919ce70b845d07719369c08c95530da30cbe4778
ikennaoyiih@gmail.com <61995586+I-GiM@users.noreply.github.com>
2020-06-25 15:00:15 +0100

    feat: encrypt-password

--- src/server.js
+++ src/server.js
@@ -1,35 +1,48 @@
 const express = require('express');
-require("./config/db");
+const bcrypt = require('bcryptjs');
 const seats = require('./models/users');
+require("./config/db");
 
 const app = express();
 app.use(express.json());
 const port = 2000;
 
 app.post('/register', async (req, res, next) => {
+    const findUser = await seats.findOne({email: req.body.email});
+
+    if (findUser) {
+       return res.json({msg: 'Email already exists!'})
+    }
     try {
-        const findUser = await seats.findOne({email: req.body.email});
+        const password = req.body.password;
+        bcrypt.genSalt(12, (err, salt) => {
+            if (err) {
+                throw err
+            } else {
+                bcrypt.hash(password, salt, async (err, encPass) => {
+                    if (err) {
+                        throw err
+                    } else {
+                        const newUser = new seats({
+                            fullname: req.body.fullname,
+                            email: req.body.email,
+                            password: encPass
+                        })
 
-        if (findUser) {
-            return res.json({msg: 'Email already exists!'})
-        }
+                        const savedUser = newUser.save();
 
-        const newUser = new seats({
-            fullname: req.body.fullname,
-            email: req.body.email,
-            password: req.body.password
+                        res.json({
+                            msg: 'User saved successfully',
+                            user: savedUser
+                        })
+                    }
+                })
+            }
         });
-
-        const savedUser = await newUser.save()
-
-        res.json({
-            msg: 'User saved successfully',
-            user: savedUser
-        })
     } catch(err) {
         res.json({msg: err});
     }
-})
+});
 
 app.listen(port, () => {
     console.log(`App listening on Port:${port}`)
